adbbb7e5f4b6a237e39f62646fce4ea09181326f
Derek Pan <dpan@advaoptical.com>
2021-04-06 17:43:47 +0800

    add package: projectile

--- init.el
+++ init.el
@@ -3,9 +3,14 @@
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
+;; Bootstrap config
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (require 'init-elpa) ;; Machinery for installing required packages
+(require 'init-exec-path) ;; Set up $PATH
 (require 'init-ui)
 (require 'init-evil)
+(require 'init-projectile)
+
+;; Load configs for specific features and modes
 
 (provide 'init)
