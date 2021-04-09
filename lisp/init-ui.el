4f3c3c2eb789ad4e466684bd191a42f9b3081836
Derek Pan <dpan@advaoptical.com>
2021-04-09 16:10:50 +0800

    tweak chinese font

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -19,5 +19,15 @@
 ;; This prevents Emacs from asking if it is safe to load the theme.
 (setq custom-safe-themes t)
 (load-theme 'modus-vivendi)
+;; (load-theme 'doom-molokai)
+
+
+;; specify font for chinese characters
+(defun font-installed-p (font-name)
+  "Check if font with FONT-NAME is available."
+  (find-font (font-spec :name font-name)))
+(cl-loop for font in '("WenQuanYi Micro Hei" "Microsoft Yahei")
+         when (font-installed-p font)
+         return (set-fontset-font t '(#x4e00 . #x9fff) font))
 
 (provide 'init-ui)
