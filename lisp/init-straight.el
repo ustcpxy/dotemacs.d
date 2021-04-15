d2b724e72e82541a3eb800d43b20270568749110
Derek Pan <dpan@advaoptical.com>
2021-04-15 17:55:07 +0800

    use straight.el as package manager instead of package.el

---
+++ lisp/init-straight.el
@@ -0,0 +1,28 @@
+;;; init-straight.el --- -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+;; Bootstrap straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(setq straight-use-package-by-default t)
+
+;; Use straight.el for use-package expressions
+(straight-use-package 'use-package)
+
+;; Load the helper package for commands like `straight-x-clean-unused-repos'
+(require 'straight-x)
+
+(provide 'init-straight)
+;;; init-straight.el ends here
