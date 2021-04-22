8dbe721086968db4e40c9d62350e8cf633a619a4
Derek Pan <dpan@advaoptical.com>
2021-04-22 18:03:47 +0800

    tweak UI, support chinese font

--- init.el
+++ init.el
@@ -5,15 +5,66 @@
 
 ;; Bootstrap config
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(require 'init-elpa) ;; Machinery for installing required packages
-(require 'init-exec-path) ;; Set up $PATH
+
+;; use straight instead of the built-in package manager
+(require 'init-straight)
+
+;;; keep ~.emacs.d~ folders clean
+(use-package no-littering)
+;; no-littering doesn't set this by default so we must place
+;; auto save files in the same path as it uses for sessions
+(setq auto-save-file-name-transforms
+      `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
+
 (require 'init-ui)
-(require 'init-evil)
-(require 'init-projectile)
 
-;; Load configs for specific features and modes
+;;; Keybinding Configuration
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-(require 'init-selectrum)
-(require 'init-org-roam)
+(use-package general
+  :after evil
+  :config
+  (general-create-definer my/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+
+  (my/leader-keys
+    "t"  '(:ignore t :which-key "toggles")
+    "tt" '(counsel-load-theme :which-key "choose theme")
+    "fde" '(lambda () (interactive) (find-file (expand-file-name "~/.emacs.d/init.el")))))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+;;; Load configs for specific features and modes
+
+
+
+
+
+
+
 
 (provide 'init)
--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -1,33 +1,90 @@
+;;; init-ui.el --- -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
 
-;; Suppress GUI features
-(setq use-file-dialog nil)
-(setq use-dialog-box nil)
+;;; Basic UI Configuration
 (setq inhibit-startup-screen t)
 
-;; disable all bar mode
-(when (fboundp 'tool-bar-mode)
-  (tool-bar-mode -1))
-(when (fboundp 'set-scroll-bar-mode)
-  (set-scroll-bar-mode nil))
-(scroll-bar-mode -1)
-(menu-bar-mode -1)
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+;; (set-fringe-mode 10)        ; Give some breathing room
 
-;; Underline line at descent position, not baseline position
-(setq x-underline-at-descent-line t)
+(menu-bar-mode -1)          ; Disable the menu bar
 
-;; Theme
+;; Set up the visible bell
+(setq visible-bell t)
+
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                treemacs-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;;; Theme
 ;; This prevents Emacs from asking if it is safe to load the theme.
 (setq custom-safe-themes t)
-(load-theme 'modus-vivendi)
-;; (load-theme 'doom-molokai)
+(use-package doom-themes
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-molokai t)
 
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
 
-;; specify font for chinese characters
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
+  (doom-themes-treemacs-config)
+
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+;;; Fonts
+;; copied from centaur's emacs
 (defun font-installed-p (font-name)
   "Check if font with FONT-NAME is available."
   (find-font (font-spec :name font-name)))
-(cl-loop for font in '("WenQuanYi Micro Hei" "Microsoft Yahei")
-         when (font-installed-p font)
-         return (set-fontset-font t '(#x4e00 . #x9fff) font))
+(when (display-graphic-p)
+  ;; Set default font
+  (cl-loop for font in '("SF Mono" "Hack" "Source Code Pro" "Fira Code"
+                         "Menlo" "Monaco" "DejaVu Sans Mono" "Consolas")
+           when (font-installed-p font)
+           return (set-face-attribute 'default nil :font font))
+
+  ;; Specify font for all unicode characters
+  (cl-loop for font in '("Apple Color Emoji" "Segoe UI Symbol" "Symbola" "Symbol")
+           when (font-installed-p font)
+           return(set-fontset-font t 'unicode font nil 'prepend))
+
+  ;; Specify font for Chinese characters
+  (cl-loop for font in '("WenQuanYi Micro Hei" "Microsoft Yahei")
+           when (font-installed-p font)
+           return (set-fontset-font t '(#x4e00 . #x9fff) font)))
+
+;;; bettor modeline
+;; NOTE: The first time you load your configuration on a new machine,
+;; you’ll need to run `M-x all-the-icons-install-fonts` so that mode line icons display correctly.
+(use-package all-the-icons)
+
+(use-package doom-modeline
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
+
+;;; which key
+(use-package which-key
+  :defer 0
+  :diminish which-key-mode
+  :config
+  (which-key-mode)
+  (setq which-key-idle-delay 1))
 
 (provide 'init-ui)
