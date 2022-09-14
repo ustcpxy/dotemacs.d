b6f126283d8187224272c24c3e5884358edabdf2
Derek Pan <DPan@adva.com>
2022-09-14 17:57:32 +0800

    changed offical melpa to netease mirrors

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -35,11 +35,13 @@
 
 ;; MELPA：软件包比 ELPA 多（软件进入 MELPA 比 ELPA 手续更简单）、新
 ;; （nightly 级别的更新速度，以时间作为版本号）
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+;; (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 ;; 稳定版 MELPA （非 nightly，有版本号）
 ;; (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 ;; org-mode 专用软件源。它几乎只服务于 org-plus-contrib 这一个包
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+
+(add-to-list 'package-archives '("melpa" . "http://mirrors.163.com/elpa/melpa/") t)
 
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
