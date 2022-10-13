91bac6238e05baf78af444e9ddaace7cd232eeb1
Derek Pan <DPan@adva.com>
2022-10-13 16:09:00 +0800

    smart entry git-commit mode

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -3,6 +3,14 @@
 (use-package magit
   :commands (magit-status)
   :config
+  (add-hook 'git-commit-setup-hook
+    (defun +vc-start-in-insert-state-maybe-h ()
+      "Start git-commit-mode in insert state if in a blank commit message,
+otherwise in default state."
+      (when (and (bound-and-true-p evil-mode)
+                 (not (evil-emacs-state-p))
+                 (bobp) (eolp))
+        (evil-insert-state))))
   )
 
 
