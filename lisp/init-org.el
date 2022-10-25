e6e8ea1b581035c14c2e9cd3d042e178c6ddbaa7
Derek Pan <DPan@adva.com>
2022-10-25 17:08:35 +0800

    custom agenda view for GTD

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -278,7 +278,30 @@ object (e.g., within a comment).  In these case, you need to use
 			((org-agenda-overriding-header "Tasks to Archive")
 			 (org-agenda-skip-function 'bh/skip-non-archivable-tasks)
 			 (org-tags-match-list-sublevels nil))))
-		 nil))))
+		 nil)
+      ("g" "Get Things Done (GTD)"
+         ((agenda ""
+                  ((org-agenda-skip-function
+                    '(org-agenda-skip-entry-if 'deadline))
+                   (org-deadline-warning-days 0)))
+          (todo "NEXT"
+                ((org-agenda-skip-function
+                  '(org-agenda-skip-entry-if 'deadline))
+                 (org-agenda-prefix-format "  %i %-12:c [%e] ")
+                 (org-agenda-overriding-header "\nTasks\n")))
+          (agenda ""
+                  ((org-agenda-entry-types '(:deadline))
+                   (org-agenda-format-date "")
+                   (org-deadline-warning-days 7)
+                   (org-agenda-skip-function
+                    '(org-agenda-skip-entry-if 'notregexp "\\* NEXT"))
+                   (org-agenda-overriding-header "\nDeadlines")))
+          (tags-todo "inbox"
+                     ((org-agenda-prefix-format "  %?-12t% s")
+                      (org-agenda-overriding-header "\nInbox\n")))
+          (tags "CLOSED>=\"<today>\""
+                ((org-agenda-overriding-header "\nCompleted today\n")))))
+		)))
 
 
   ;; NEXT is only for tasks
