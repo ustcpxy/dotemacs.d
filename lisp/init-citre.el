d2bdfb2b844e16b27f9f28d1a64c8d71e515d811
Derek Pan <DPan@adva.com>
2023-05-10 10:35:01 +0800

    setup citre

--- lisp/init-citre.el
+++ lisp/init-citre.el
@@ -9,5 +9,24 @@
               imenu-alist))
 (advice-add 'citre-imenu-create-index-function :filter-return #'filter-imenu)
 
+;; @https://github.com/universal-ctags/citre/wiki/Useful-commands
+(defun citre-jump+ ()
+  (interactive)
+  (condition-case _
+      (citre-jump)
+    (error (let* ((xref-prompt-for-identifier nil))
+             (call-interactively #'xref-find-definitions)))))
+
+(defun my--push-point-to-xref-marker-stack (&rest r)
+  (xref-push-marker-stack (point-marker)))
+(dolist (func '(find-function
+                counsel-imenu
+                helm-imenu
+                projectile-grep
+                helm-grep-ag
+                counsel-rg
+                lsp-ivy-workspace-symbol
+                citre-jump))
+  (advice-add func :before 'my--push-point-to-xref-marker-stack))
 
 (provide 'init-citre)
