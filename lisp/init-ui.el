84632db5136e3f8e488a952ce9f7581fd5945966
Derek Pan <DPan@adva.com>
2024-04-18 11:56:18 +0800

    update font size

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -89,7 +89,7 @@
 (defun font-installed-p (font-name)
   "Check if font with FONT-NAME is available."
   (find-font (font-spec :name font-name)))
-(when (display-graphic-p)
+;;(when (display-graphic-p)
   ;; Set default font
   ;; (cl-loop for font in '("Cascadia Code" "Fira Code" "Jetbrains Mono"
   ;;                        "SF Mono" "Hack" "Source Code Pro" "Menlo"
@@ -99,7 +99,7 @@
   ;; font family candidate ("DejaVu Sans Mono" "Source Code Pro")
   ;; (set-face-attribute 'default nil :font (font-spec :family "monospace" :size 16))
   ;;(set-face-attribute 'default nil :font (font-spec :family "DejaVu Sans Mono") :height 150)
-  (set-face-attribute 'default nil :font (font-spec :family "Microsoft Yahei") :height 150)
+;;  (set-face-attribute 'default nil :font (font-spec :family "Microsoft Yahei") :height 150)
   
   ;; Specify font for all unicode characters
   ;; (cl-loop for font in '("Segoe UI Symbol" "Symbola" "Symbol")
@@ -124,9 +124,56 @@
   ;;          return (progn
   ;;                   (setq face-font-rescale-alist `((,font . 1.3)))
   ;;                   (set-fontset-font t '(#x4e00 . #x9fff) (font-spec :family font))))
+;; (if (eq system-type 'windows-nt)
+;;  (set-face-attribute 'default nil :font (font-spec :family "Microsoft Yahei") :height 150)
 
-  ;; (set-fontset-font t 'han "WenQuanYi Micro Hei")
-)
+;;    (set-fontset-font t 'han "WenQuanYi Micro Hei"))
+;; )
+
+;; copied @https://emacs-china.org/t/emacs/22193
+(defun centaur-setup-fonts ()
+  "Setup fonts."
+  (when (display-graphic-p)
+    ;; Set default font
+    (cl-loop for font in '("Cascadia Code" "Fira Code" "Jetbrains Mono"
+                           "SF Mono" "Hack" "Source Code Pro" "Menlo"
+                           "Monaco" "DejaVu Sans Mono" "Consolas")
+             when (font-installed-p font)
+             return (set-face-attribute 'default nil
+                                        :family font
+                                        :height 150))
+
+    ;; Set mode-line font
+    ;; (cl-loop for font in '("Menlo" "SF Pro Display" "Helvetica")
+    ;;          when (font-installed-p font)
+    ;;          return (progn
+    ;;                   (set-face-attribute 'mode-line nil :family font :height 120)
+    ;;                   (when (facep 'mode-line-active)
+    ;;                     (set-face-attribute 'mode-line-active nil :family font :height 120))
+    ;;                   (set-face-attribute 'mode-line-inactive nil :family font :height 120)))
+
+    ;; Specify font for all unicode characters
+    (cl-loop for font in '("Segoe UI Symbol" "Symbola" "Symbol")
+             when (font-installed-p font)
+             return (set-fontset-font t 'unicode font nil 'prepend))
+
+    ;; Emoji
+    (cl-loop for font in '("Noto Color Emoji" "Apple Color Emoji")
+             when (font-installed-p font)
+             return (if (>= emacs-major-version 28)
+                        (set-fontset-font t 'emoji (font-spec :family font) nil 'prepend)
+                      (set-fontset-font t 'symbol (font-spec :family font) nil 'prepend)))
+
+    ;; Specify font for Chinese characters
+    (cl-loop for font in '("WenQuanYi Micro Hei" "PingFang SC" "Microsoft Yahei" "STFangsong")
+             when (font-installed-p font)
+             return (progn
+                      (setq face-font-rescale-alist `((,font . 1.3)))
+                      (set-fontset-font t '(#x4e00 . #x9fff) (font-spec :family font))))))
+
+(centaur-setup-fonts)
+(add-hook 'window-setup-hook #'centaur-setup-fonts)
+(add-hook 'server-after-make-frame-hook #'centaur-setup-fonts)
 
 ;; Easily adjust the font size in all frames
 (use-package default-text-scale
